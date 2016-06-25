b2f5d008cfc224d39cbe0a7de9835246520b8dd0
Przemo Nowaczyk <pnowaczyk.mail@gmail.com>
2016-06-25 11:32:20 +0200

    move tests to pytest

--- incubator/test/test_hello.py
+++ incubator/test/test_hello.py
@@ -1,14 +1,8 @@
-import unittest
-
 from incubator.hello import Hello
 
 
-class TestHello(unittest.TestCase):
+class TestHello():
     def test_say_contains_name(self):
         name = 'FRIDAY replacement for J.A.R.V.I.S'
         h = Hello(name)
-        self.assertIn(name, h.say())
-
-
-if __name__ == '__main__':
-    unittest.main()
+        assert name in h.say()
